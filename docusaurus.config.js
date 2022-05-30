b1999a614d3bba2d8e971084d7b7cb979dc51dbc
Dan Lee <dan-lee1@hotmail.co.uk>
2022-05-30 22:11:08 +0100

    katex support test

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -3,6 +3,9 @@
 
 const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const math = require('remark-math');
+const katex = require('rehype-katex');
+
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -35,6 +38,9 @@ const config = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           routeBasePath: '/',
+          remarkPlugins: [math],
+          rehypePlugins: [katex],
+          path: 'docs',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
         },
@@ -44,6 +50,16 @@ const config = {
         },
       }),
     ],
+  ],
+
+  stylesheets: [
+    {
+      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
+      type: 'text/css',
+      integrity:
+        'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
+      crossorigin: 'anonymous',
+    },
   ],
 
   themeConfig:
